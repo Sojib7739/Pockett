d11b4f634641da304c00e610af04b9aaa899ce97
Sameen Irtisam <sameenirtisam0@gmail.com>
2026-03-05 06:24:36 +0600

    nn

--- lib/reminder.dart
+++ lib/reminder.dart
@@ -1,25 +1,144 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
-class Reminder extends StatefulWidget {
+class Reminder extends StatelessWidget {
   const Reminder({super.key});
 
-  @override
-  State<Reminder> createState() => _ReminderState();
-}
-
-class _ReminderState extends State<Reminder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      appBar: AppBar(
-        backgroundColor: klinearGradientStart,
-        title: Text("Reminder"),
-      ),
-      body: Center(
-        child: Text("Reminder Page"),
+      appBar: appBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text('Payment Name', style: TextStyle(fontSize: 16)),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Amount", style: TextStyle(fontSize: 16)),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ),
+          ),
+
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Date", style: TextStyle(fontSize: 16)),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20, top: 20),
+            child: Text("Merchant", style: TextStyle(fontSize: 16)),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 100),
+              child: GestureDetector(
+                onTap: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Reminder Saved")),
+                  );
+                },
+                child: Container(
+                  width: 200,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: klinearGradientEnd,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "Save",
+                    style: kWhiteBold.copyWith(fontSize: 24),
+
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: klinearGradientStart,
+      title: const Text('Reminder'),
+      centerTitle: true,
+    );
+  }
 }
+
+
+
